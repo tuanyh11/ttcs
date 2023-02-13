1edc2103a0a1e8bd84883bc2e9219f755adfc523
tuan <tuan leo>
2023-02-13 20:14:28 +0700

    tuan commit make header

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,6 +1,64 @@
-const data = [
-    {
-        id: 1,
-        
-    }
-]
+const data = {
+  menus: {
+    nodes: [
+      {
+        name: "Custom Links",
+        id: "dGVybToxMTU=",
+        menuItems: {
+          nodes: [
+            {
+              label: "Legal Notice",
+              childItems: {
+                nodes: [],
+              },
+            },
+            {
+              label: "Prices Drop",
+              childItems: {
+                nodes: [],
+              },
+            },
+            {
+              label: "New Products",
+              childItems: {
+                nodes: [
+                  {
+                    label: "Best Sales",
+                    childItems: {
+                      edges: [
+                        {
+                          node: {
+                            label: "My account",
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              label: "Best Sales",
+              childItems: {
+                nodes: [
+                  {
+                    label: "My account",
+                    childItems: {
+                      edges: [],
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              label: "My account",
+              childItems: {
+                nodes: [],
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
